0e2fa6a75d65a8b6423f8b86a0b0ea9215c0e4db
IsidroGP <33144001@uaz.edu.mx>
2020-01-30 11:58:35 -0600

    La tercera

---
+++ 03.areatriangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03.areatriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que calcula el area de un triangulo");
+            double labase, altura, area;
+
+            Console.WriteLine("Dame la base: ");
+            labase = float.Parse(Console.ReadLine());
+            Console.WriteLine("Dame la altura");
+            altura = double.Parse(Console.ReadLine());
+
+            area = (labase*altura)/2;
+
+            Console.Write($"El area es {area}");
+            Console.ReadLine();
+        }
+    }
+}
